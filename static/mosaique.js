08e457c491b731971adecd5b83da7e85761c3882
Thibault Raffaillac <traf@kth.se>
2020-10-30 20:25:07 +0100

    Implémentation des logiques de base des éléments interactifs

--- static/mosaique.js
+++ static/mosaique.js
@@ -27,7 +27,7 @@ function maj_mosaique() {
 			while (div_mosaique.children.length > apprenants.length)
 				div_mosaique.lastChild.remove()
 			while (div_mosaique.children.length < apprenants.length)
-				div_mosaique.insertAdjacentHTML('beforeend', '<div><span>Test</span></div>')
+				div_mosaique.insertAdjacentHTML('beforeend', '<div onclick="focus_apprenant(this.children[0].innerText)"><span>Test</span></div>')
 		}
 		
 		// on transfère les données des apprenants vers les cellules de la grille
@@ -57,7 +57,53 @@ maj_mosaique()
 
 
 
-// ancien code
+// passage de la mosaïque à la vue apprenant
+function focus_apprenant(nom_apprenant) {
+	div_overlay.style.display = 'flex'
+	lbl_nom_apprenant.innerText = nom_apprenant
+}
+
+
+
+// gestion des paramètres en vue apprenant
+inp_theme.value = localStorage.getItem('inp_theme') || 'xcode'
+inp_theme.onchange = () => {
+	ace_editeur.setTheme(`ace/theme/${inp_theme.value}`)
+	localStorage.setItem('inp_theme', inp_theme.value)
+}
+
+
+
+// commande de fin d'assistance en vue apprenant
+btn_fin_assistance.onclick = () => {
+	if (btn_fin_assistance.classList.contains('checked'))
+		btn_fin_assistance.classList.replace('checked', 'unchecking')
+}
+
+
+
+// initialisation de l'éditeur en vue apprenant
+ace.config.set('basePath', 'https://pagecdn.io/lib/ace/1.4.12/')
+const ace_editeur = ace.edit('txt_editeur', {
+	mode: 'ace/mode/python',
+	readOnly: true,
+	theme: `ace/theme/${localStorage.getItem('inp_theme') || 'xcode'}`,
+})
+
+
+
+// initialisation du séparateur mobile en vue apprenant
+Split(['#div_gauche', '#div_droite'], {
+	sizes: JSON.parse(localStorage.getItem('split_sizes') || '[50,50]'),
+	gutterSize: 4,
+	snapOffset: 0,
+	elementStyle: (_, s, g) => ({'flex-basis': `calc(${s}% - ${g}px)`}),
+	gutterStyle: (_, g) => ({'flex-basis': `${g}px`}),
+	onDragEnd: (s) => localStorage.setItem('split_sizes', JSON.stringify(s)),
+})
+
+
+
 /*const ajax = new XMLHttpRequest()
 const ace_modelist = ace.require('ace/ext/modelist')
 const ace_editeur = ace.edit('txt_editeur')
